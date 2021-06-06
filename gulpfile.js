812a0053cbf7037d416c5249878bea2875d8d6f4
Tristan Brookes <tristan.s.brookes@gmail.com>
2021-06-06 22:46:45 +1000

    Added a zip function. Added the zip file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 const os = require('os');
 const gulp = require('gulp');
+const zip = require('gulp-zip');
 
 function copyToWidgetsFolder() {
   return gulp.src(['widget/**/*'])
     .pipe(gulp.dest('Library/Application\ Support/Übersicht/widgets/taylor-swift-quote.widget/', { cwd: os.homedir }));
 }
 
-exports.default = copyToWidgetsFolder
+function zipIt() {
+  return gulp.src('widget/**/*')
+    .pipe(zip('taylor-swift-quote.widget.zip'))
+    .pipe(gulp.dest('./'))
+}
+
+exports.default = copyToWidgetsFolder;
+exports.build = zipIt;
